e3241f218473b507fcac40928ca72f370eedb326
GertRL <92432784+GertRL@users.noreply.github.com>
2022-10-02 21:57:14 +0300

    5.0

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
             // solution
-            url: 'https://www.vuemastery.com/'
+            sizes: ['S', 'M', 'L', 'XL'],
             // solution
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
